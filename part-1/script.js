a81501df0f04c83e24c70969bed38fcd126e72fa
Summy Singh <summysingh86@gmail.com>
2021-04-03 17:40:56 +0530

    added

---
+++ part-1/script.js
@@ -0,0 +1,62 @@
+/*let js = 'amazing';
+
+let firstName="summy";
+let lastName = "singh";
+console.log("First name is: "+firstName);
+console.log("Last Name is: "+lastName);
+console.log("Full Name is: "+firstName +" "+lastName);
+*/
+
+// let jsBool = true;
+// console.log(jsBool);
+
+// console.log(typeof(jsBool));
+// console.log(typeof jsBool);
+// console.log(typeof true);
+// console.log(typeof 3);
+// console.log(typeof(3));
+// console.log(typeof NaN);
+// console.log(typeof(NaN));
+// console.log(typeof null);
+// console.log(typeof(null));
+// console.log(typeof string);
+// console.log(typeof 'summy');
+// let jsIs;
+// console.log(typeof jsIs);
+// console.log(typeof 3.14);
+// console.log(typeof '');
+// console.log(typeof(NaN));
+// console.log(typeof null)
+
+// const year=2020
+// const ageSummy = year-1996;
+// const ageSunny=year-1993;
+// console.log(ageSummy, ageSunny);
+
+// console.log(ageSummy*2, ageSummy+10, ageSummy/2, ageSummy%2);
+// console.log(ageSummy*2);
+// console.log(ageSummy+10);
+// console.log(ageSummy/2);
+// console.log(ageSummy%5);
+// console.log(2**3);//mean 2 to the power 3
+
+
+//assignmnet opperator
+// let x=10+15;
+// console.log(x);
+// x+=10;
+// console.log(x);
+// x-=10;
+// console.log(x);
+// x*=10;
+// console.log(x);
+// x++;
+// console.log(x);
+// x--;
+// console.log(x);
+// ++x;
+// console.log(x);
+// --x;
+// console.log(x)
+
+//conparison operator >,<,>=,<= it will return true or false
