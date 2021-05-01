496c304c3baefcbce7eef78d49c0fdde1c8954d9
Summy Singh <summysingh86@gmail.com>
2021-05-02 05:22:10 +0530

    array added

---
+++ Array/script.js
@@ -0,0 +1,193 @@
+'use strict';
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// BANKIST APP
+
+// Data
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
+};
+
+const account2 = {
+  owner: 'Jessica Davis',
+  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+  interestRate: 1.5,
+  pin: 2222,
+};
+
+const account3 = {
+  owner: 'Steven Thomas Williams',
+  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  interestRate: 0.7,
+  pin: 3333,
+};
+
+const account4 = {
+  owner: 'Sarah Smith',
+  movements: [430, 1000, 700, 50, 90],
+  interestRate: 1,
+  pin: 4444,
+};
+
+const accounts = [account1, account2, account3, account4];
+
+// Elements
+const labelWelcome = document.querySelector('.welcome');
+const labelDate = document.querySelector('.date');
+const labelBalance = document.querySelector('.balance__value');
+const labelSumIn = document.querySelector('.summary__value--in');
+const labelSumOut = document.querySelector('.summary__value--out');
+const labelSumInterest = document.querySelector('.summary__value--interest');
+const labelTimer = document.querySelector('.timer');
+
+const containerApp = document.querySelector('.app');
+const containerMovements = document.querySelector('.movements');
+
+const btnLogin = document.querySelector('.login__btn');
+const btnTransfer = document.querySelector('.form__btn--transfer');
+const btnLoan = document.querySelector('.form__btn--loan');
+const btnClose = document.querySelector('.form__btn--close');
+const btnSort = document.querySelector('.btn--sort');
+
+const inputLoginUsername = document.querySelector('.login__input--user');
+const inputLoginPin = document.querySelector('.login__input--pin');
+const inputTransferTo = document.querySelector('.form__input--to');
+const inputTransferAmount = document.querySelector('.form__input--amount');
+const inputLoanAmount = document.querySelector('.form__input--loan-amount');
+const inputCloseUsername = document.querySelector('.form__input--user');
+const inputClosePin = document.querySelector('.form__input--pin');
+
+/////////////////////////////////////////////////
+/////////////////////////////////////////////////
+// LECTURES
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+//const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+/////////////////////////////////////////////////
+
+
+//////Basic Array Method
+
+//let arr = ['a','b','c','d','e','f'];
+
+// console.log(arr);//a,b,c,d,e,f
+
+//Slice
+// console.log(arr.slice(2));//c,d,e,f
+// console.log(arr.slice(2,4));//c,d
+// console.log(arr.slice(-2));//e,f
+// console.log(arr.slice(-1));//f
+// console.log(arr.slice(1,-2));//b,c,d
+// //shallow copy of arrow
+// console.log(arr.slice());//a,b,c,d,e,f
+// console.log([...arr]);//a,b,c,d,e,f
+
+
+//Splice
+// console.log(arr.splice(2)); //c,d,e,f
+// console.log(arr);//a,b
+
+// console.log(arr.splice(-1));    //f
+// console.log(arr); //a,b,c,d,e
+
+// console.log(arr.splice(1,2));   //b,c
+// console.log(arr);//a,d,e,f
+
+//Reverse
+
+//const arr2 = ['j','i','h','g','f'];
+// console.log(arr2);//j,i,h,g,f
+// console.log(arr2.reverse());//f,g,h,i,j
+// console.log(arr2);//f,g,h,i,j
+
+// //CONCAT
+
+// const letters = arr.concat(arr2);
+// console.log(letters);//a,b,c,d,e,f,f,g,h,i,j
+
+// console.log([...arr,...arr2]);//a,b,c,d,e,f,f,g,h,i,j
+
+// //JOIN
+// console.log(letters.join('-'));//a-b-c-d-e-f-f-g-h-i-j
+
+
+
+//For each
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//for of
+// for(const movement of movements){
+//     if(movement>0){
+//         console.log(`You deposited ${movement}`)
+//     }else{
+//         console.log(`You withdrew ${Math.abs(movement)}`);
+//     }
+// }
+
+//accesing entries with for-of
+// for(const [i, movement] of movements.entries()){
+//     if(movement>0){
+//         console.log(`Movement ${i+1} You deposited ${movement}`)
+//     }else{
+//         console.log(`Movement ${i+1} You withdrew ${Math.abs(movement)}`);
+//     }
+// }
+
+// //for each
+//  console.log('---For Each---')
+// // movements.forEach(function(movement){
+// //     if(movement>0){
+// //         console.log(`You deposited ${movement}`)
+// //     }else{
+// //         console.log(`You withdrew ${Math.abs(movement)}`);
+// //     }
+// // })
+
+// //for-each entries
+
+// movements.forEach(function(movement,i, arr){
+//     if(movement>0){
+//         console.log(`Movement ${i+1} You deposited ${movement}`)
+//     }else{
+//         console.log(`Movement ${i+1} You withdrew ${Math.abs(movement)}`);
+//     }
+// })
+
+// //0: function(200)
+// //1: function(450)
+// //2: function(400)
+// //...
+
+
+//For Each with Map and Set
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+
+// //map
+// currencies.forEach(function(value, key, map){
+//     console.log(`${key}:${value}`);
+// });
+
+// //set
+// const currenciesUnique = new Set(['USD','GBP','USD','EUR','EUR']);
+// console.log(currenciesUnique);
+
+// currenciesUnique.forEach(function(value, _, map){
+//     console.log(`${value}:${value}`);
+// })
